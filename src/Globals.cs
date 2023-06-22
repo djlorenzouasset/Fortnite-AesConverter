c12fb6709caadb32f8b2c2c560664bd8d5d735c5
ᴅᴊʟᴏʀ3xᴢᴏ <lorenzogame85@gmail.com>
2023-06-22 23:37:17 +0200

    missed a wrong link

--- src/Globals.cs
+++ src/Globals.cs
@@ -3,6 +3,6 @@
 public static class Globals
 {
     public const string DISCORD = "https://discord.gg/BreVGmCdef";
-    public const string GITHUB = "https://github.com/djlorenzouasset";
+    public const string GITHUB = "https://github.com/djlorenzouasset/Fortnite-AesConverter";
     public const string KEYCHAINARCHIVE = "https://api.nitestats.com/v1/epic/keychain";
 }
